608749dab23a138b2ae93c8ce32677b06444203d
Max <maks.fomichev.94@mail.ru>
2023-07-01 13:19:03 +0400

    add cfg

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,20 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+import { BuildPaths } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildPlugins({
+  html,
+}: BuildPaths): webpack.WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+  ];
+}
